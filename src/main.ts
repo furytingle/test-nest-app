1538100f64dd0e5ce4e3f7750c170ca81ec9e213
oleksandr.zhylisnkyi <oleksandr.zhylinskyi@wiz-team.com>
2026-02-10 15:37:38 +0200

    certificates

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConsoleLogger, ValidationPipe } from '@nestjs/common';
+import * as fs from 'node:fs';
 
 async function bootstrap() {
+  const httpsOptions = {
+    key: fs.readFileSync('../secrets/t-self-signed-private.pem'),
+    cert: fs.readFileSync('../secrets/t-self-signed-public.pem'),
+  };
+
   const app = await NestFactory.create(AppModule, {
+    httpsOptions,
     logger: new ConsoleLogger(),
   });
+
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);

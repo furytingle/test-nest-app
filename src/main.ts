5915aefa4c74ed43cda01f25001077a2345d7668
oleksandr.zhylisnkyi <oleksandr.zhylinskyi@wiz-team.com>
2026-02-11 15:03:22 +0200

    secrets correct path

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ import * as fs from 'node:fs';
 
 async function bootstrap() {
   const httpsOptions = {
-    key: fs.readFileSync('secrets/t-self-signed-private.key'),
-    cert: fs.readFileSync('secrets/t-self-signed-public.pem'),
+    key: fs.readFileSync('./secrets/t-self-signed-private.key'),
+    cert: fs.readFileSync('./secrets/t-self-signed-public.pem'),
   };
 
   const app = await NestFactory.create(AppModule, {

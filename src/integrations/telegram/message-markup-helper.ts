3a71ec0263c76f730dae222be909af831f04a583
oleksandr.zhylisnkyi <oleksandr.zhylinskyi@wiz-team.com>
2026-02-25 13:42:03 +0200

    markup

--- src/integrations/telegram/message-markup-helper.ts
+++ src/integrations/telegram/message-markup-helper.ts
@@ -2,6 +2,6 @@ import { CurrentWeather } from '../weather/current-weather';
 
 export class MessageMarkupHelper {
   static formatCurrentWeather(currentWeather: CurrentWeather): string {
-    return `Currently: ${currentWeather.temperature}&#8451;%0AHumidity: ${currentWeather.humidity}% %0AWind speed: ${currentWeather.windSpeed} m/s %0ARain: ${currentWeather.rain}%`;
+    return `Currently: ${currentWeather.temperature}&#8451;\nHumidity: ${currentWeather.humidity}% Wind speed: ${currentWeather.windSpeed} m/s Rain: ${currentWeather.rain}%`;
   }
 }
